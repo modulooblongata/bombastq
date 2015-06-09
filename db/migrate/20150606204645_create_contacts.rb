f2aa625f1869e42ece6b0fc06452cc4f4fd72c00
Romina Halabi <unprevailing@gmail.com>
2015-06-09 16:25:15 +0000

    Added contacts table and model

--- db/migrate/20150606204645_create_contacts.rb
+++ db/migrate/20150606204645_create_contacts.rb
@@ -1,10 +1,9 @@
-class CreateContacts < ActiveRecord::Migration
+cclass CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
       t.string :name
       t.string :email
       t.text :comments
-      
       t.timestamps
     end
   end
