336255f065973651c4a5247aa42e590548562505
Romina Halabi <unprevailing@gmail.com>
2015-06-10 19:36:59 +0000

    added validations and create action

--- app/models/concerns/contact.rb
+++ app/models/concerns/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ActiveRecord::Base
+  validates :name, :email, :comments, presence: true
+  validates :comments, length: { maximum: 1000,
+    too_long: "%{count} characters is the maximum allowed." }
 end
